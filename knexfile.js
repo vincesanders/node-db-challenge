39bdf73372cbb9859e401f5122609b8ee17d920a
Vincent Sanders <vincesanders@hotmail.com>
2020-02-21 10:13:57 -0600

    Corrected typo in database name. Created tables.

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/project.db3'
+      filename: './data/projects.db3'
     },
     useNullAsDefault: true,
     pool: {
