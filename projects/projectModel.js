18713fb0b82253f6dd2005297dde81a140ddeccb
Vincent Sanders <vincesanders@hotmail.com>
2020-02-21 11:37:55 -0600

    Added endpoints for getting all resources and for getting projects, resources and tasks by project id.

--- projects/projectModel.js
+++ projects/projectModel.js
@@ -1,9 +1,36 @@
 const db = require('../data/db-config');
 
 module.exports = {
-    getProjects
+    getProjects,
+    getAllResources,
+    getProjectById,
+    getResourcesByProjectId,
+    getTaskByProjectId
 }
 
 function getProjects() {
     return db('project');
+}
+
+function getProjectById(id) {
+    return db('project').where({ id });
+}
+
+function getAllResources() {
+    return db('resource');
+}
+
+function getResourcesByProjectId(id) {
+    return db('project_resources as pr')
+        .join('resource as r', 'pr.resource_id', 'r.id')
+        .join('project as p', 'pr.project_id', 'p.id')
+        .select('p.name as project-name', 'r.id', 'r.name as resource-name', 'r.description')
+        .where('pr.project_id', id);
+}
+
+function getTaskByProjectId(id) {
+    return db('task as t')
+        .join('project as p', 't.project_id', 'p.id')
+        .select('p.name as project-name', 'p.description as project-description', 't.id', 't.description', 't.notes')
+        .where('t.project_id', id);
 }
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -12,4 +12,53 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    projects.getProjectById(req.params.id).then(projects => {
+        if (projects.length > 0) {
+            res.status(200).json(projects[0]);
+        } else {
+            res.status(404).json({ message: 'Unable to retrieve the project you requested' });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'Unable to retrieve the project requested.'});
+    });
+});
+
+//In a real project, I would put this in a different router.
+router.get('/resources', (req, res) => {
+    projects.getAllResources().then(resources => {
+        res.status(200).json(resources);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'Unable to retrieve a list of resources.'});
+    });
+});
+
+router.get('/:id/resources', (req, res) => {
+    projects.getResourcesByProjectId(req.params.id).then(resources => {
+        if (resources.length > 0) {
+            res.status(200).json(resources);
+        } else {
+            res.status(404).json({ message: 'Unable to retrieve the resources for the project you requested' });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'Unable to retrieve the resources requested.'});
+    });
+});
+
+router.get('/:id/tasks', (req, res) => {
+    projects.getTaskByProjectId(req.params.id).then(tasks => {
+        if (tasks.length > 0) {
+            res.status(200).json(tasks);
+        } else {
+            res.status(404).json({ message: 'Unable to retrieve the tasks for the project you requested' });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'Unable to retrieve the tasks requested.'});
+    });
+});
+
 module.exports = router;
